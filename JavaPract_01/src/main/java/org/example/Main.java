70d49a5df2762b1d9a0d7a5c9b93b8b7cb4464c1
Carrington-dev <crn96m@gmail.com>
2025-01-31 13:04:27 +0200

    Added Java Array Access

--- JavaPract_01/src/main/java/org/example/Main.java
+++ JavaPract_01/src/main/java/org/example/Main.java
@@ -13,5 +13,12 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
+
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for(int i = 0; i < numbers.length; i++){
+            System.out.println("number[i] = " + numbers[i]);
+        }
     }
 }
