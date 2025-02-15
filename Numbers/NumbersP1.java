80d7db4d64dfbbdd7e21413c0a9b1f44afad2370
Carrington-dev <crn96m@gmail.com>
2025-02-15 20:09:52 +0200

    Added op

--- Numbers/NumbersP1.java
+++ Numbers/NumbersP1.java
@@ -1,6 +1,11 @@
 class Main{
     public static void main(String[] args) {
         System.out.println(5);
+        System.out.println(5 / 2);
+        System.out.println(5 + 2);
+        System.out.println(5 - 2);
+        System.out.println(5 * 2);
+        System.out.println(5 * 2);
         System.out.println("5");
     }
 }
